68b4628dab859821eb57f29fca3f3ae8fda844ed
“faizanalibaig” <riazfiaz6181@gmail.com>
2024-07-01 03:08:51 +0500

    bg change

--- components/articlesection/ArticleSection.js
+++ components/articlesection/ArticleSection.js
@@ -6,7 +6,7 @@ import Link from 'next/link';
 const ArticleCard = () => (
   <div className='bg-black text-white h-[375px] lg:h-[380px] xl:h-[400px] sm:w-[350px] lg:w-[300px] xl:w-[340px] border border-black/10 px-3 py-3 flex flex-col justify-between shadow-[0_2px_4px_rgba(0,0,0,0.05)]'>
     <section className='h-full flex flex-col gap-3'>
-      <div className='h-[55%] bg-gray-50' />
+      <div className='h-[55%] bg-gray-50/20' />
       <div className='flex flex-col gap-2'>
         <h1 className='font-semibold text-[16px] lg:text-[18px] xl:text-[19px]'>
           bluehost - up to 70% off
